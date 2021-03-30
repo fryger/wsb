4e2f09606408c49d51644050d8bbe7675b57b261
kfryg <fryg3r@gmail.com>
2021-03-30 22:48:27 +0200

    car, gps base model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Car(models.Model):
+    fname = models.CharField(max_length=255)
+    manufacturer = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    mileage = models.PositiveIntegerField()
+    vin = models.CharField(max_length=17)
+
+    def __str__(self):
+        return self.fname
+
+class Gps(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    datetime = models.DateTimeField(default=datetime.now)
--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Car, Gps
 
-# Create your views here.
+def index(response, id):
+    cars = Car.objects.get(id=id)
+    path = cars.gps_set.all()
+    return HttpResponse(f"<p>{cars.fname}</p><p>{[i.lat for i in path]}</p>")
