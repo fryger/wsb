00022eaf5fe02b53c9891fb500a3d0339acff8a7
kfryg <fryg3r@gmail.com>
2021-04-07 22:00:32 +0200

    REST API basics and tests, frontend base, basic token auth

--- home/views.py
+++ home/views.py
@@ -1,8 +1,81 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Car, Gps
+from .serializers import CarSerialzer
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-def index(response, id):
-    cars = Car.objects.get(id=id)
-    path = cars.gps_set.all()
-    return HttpResponse(f"<p>{cars.fname}</p><p>{[i.lat for i in path]}</p>")
+
+# Do ogarniecia ViewSet ModelViewSet
+# tut https://www.youtube.com/watch?v=B38aDwUpcFc&t=927s
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = CarSerialzer
+    queryset = Car.objects.all()
+    lookup_field = 'id'
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
+
+class MapView(APIView):
+    def get(self, request):
+        cars = Car.objects.all()
+        serializer = CarSerialzer(cars, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CarSerialzer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CarDetail(APIView):
+    def get_object(self, id):
+        try:
+            return Car.objects.get(id=id)
+        except Car.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        car = self.get_object(id)
+        serializer = CarSerialzer(car)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        car = self.get_object(id)
+        serializer = CarSerialzer(car, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        car = self.get_object(id)
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
