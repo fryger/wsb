158851f32932e009cb8ef2e3076460841a34a16f
Krzysztof <krzysztof@fryger.pl>
2021-05-08 16:08:21 +0200

    Added basic login, register

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path, include
 from .views import UserCreation, CarCollection, CarDetails, GpsCollection
-from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path('login', obtain_auth_token),
+    path('register', UserCreation.as_view()),
     path('cars', CarCollection.as_view()),
     path('cars/<int:pk>/', CarDetails.as_view()),
     path('cars/<int:pk>/gps', GpsCollection.as_view()),
